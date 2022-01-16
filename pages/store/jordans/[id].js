cb3571d1bb59a22663d0dc6c5c9a72e65f06da46
liferacer23 <binyamg23@gmail.com>
2022-01-16 15:52:38 +0300

    id page modifications

--- pages/store/jordans/[id].js
+++ pages/store/jordans/[id].js
@@ -5,38 +5,60 @@ import styles from "../../../styles/SelectedJordan.module.css";
 export default function SelectedJordan({ jordans }) {
   return (
     <div className={styles.container}>
-      
-        <div className={styles.imageSection}>
-          {jordans.image.map((data, index) => {
-            return (
-              <div className={styles.image}key={index}>
-                <Image src={data} width={350} height={350} />
-              </div>
-            );
-          })}
-        </div>
-      
-      <div className={styles.infoSection}>
-        <h1 className={styles.title}>{jordans.title}</h1>
-        <p>{jordans.description}</p>
-        {jordans.prices.map((price, index) => {
+      <div className={styles.imageSection}>
+        {jordans.image.map((data, index) => {
           return (
-            <div key={index} className={styles.prices}>
-              <span><p>{price}$</p></span>
+            <div className={styles.image} key={index}>
+              <Image src={data} width={350} height={350} />
             </div>
           );
         })}
-        <h3>Extra:</h3>
-        {jordans.extraOptions.map((data,index)=>{
-            return (
-              <div>
-                
-                <h3>{data.text}</h3>
-                <h3>{data.price}%</h3>
-              </div>
-              )
-        })}
       </div>
+
+<form className={styles.form}>
+      <div className={styles.infoSection}>
+        <h1 className={styles.title}>{jordans.title}</h1>
+        <p>{jordans.description}</p>
+
+          <h3>Please select your size</h3>
+          <div>
+            {jordans.prices.map((price, index) => {
+              let pricerange1=6;
+              let pricerange2=10;
+              if (index===1){
+                pricerange1+=4
+                pricerange2+=2
+              }else if (index===2){
+                pricerange1+=6
+                pricerange2+=5
+              }
+              return (
+                <div key={index} className={styles.prices}>
+                  <input
+                    type="radio"
+                    id={index}
+                    name="contact"
+                    value={price}
+                  />
+                  <label htmlFor="contactChoice3">Size {pricerange1}-{pricerange2} {price}$</label>
+                </div>
+              );
+            })}
+          </div>
+          {jordans.extraOptions.map((data, index) => {
+          return (
+            <div key={index}>
+              <h3>{data.text}</h3>
+              <h3>{data.price}$</h3>
+            </div>
+          );
+        })}
+          <div>
+            <button type="submit">Add To Cart</button>
+          </div>
+    
+      </div>
+        </form>
     </div>
   );
 }
